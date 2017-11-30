3750ef8c44c75b39fafcd51db304e9d4cf8a4375
Jarrod <jarrod.miller@ttu.edu>
2017-11-30 10:28:48 -0700

    Added env variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,7 @@ import { browserHistory } from 'react-router';
 import {AUTH_USER, UNAUTH_USER, GET_POLLS, GET_POLL, CREATE_POLL, VOTE_POLL, ANON_USER, ERROR, USER_POLLS, DELETE_POLL, ADD_OPTION} from './types';
 var twitterAPI = require('node-twitter-api');
 
-//const ROOT_URL = 'https://pacific-scrubland-65914.herokuapp.com'; //3090
-const ROOT_URL = 'http://localhost:5000';
+const ROOT_URL =  process.env.DB_PATH ? process.env.DB_PATH : 'http://localhost:5000'; 
 
 export function signinUser({email, password}) {
   const userEmail = {email, password}.email;
